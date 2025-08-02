7a7a4c0a9bb9e077ce39189a288a818172229cc4
Kergrit Robkop <kergrit@gmail.com>
2025-08-02 18:26:42 +0700

    feat: add CI/CD text to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li>CI/CD with connect github repository to cloud run</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
